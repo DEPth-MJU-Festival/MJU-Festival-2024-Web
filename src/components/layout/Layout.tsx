2d9f61efaad2ac867c1962f54b10fd03be3a4e77
rlotr02 <0314kelee@naver.com>
2024-10-01 02:46:21 +0900

    fix: 주소 표시줄 헤더 사이 공백 제거

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,10 +3,24 @@ import * as S from '@styles/layout/LayoutStyle';
 import BannerImg from '@images/main/Banner.png';
 import Header from './Header';
 import Footer from './Footer';
+import { useEffect, useState } from 'react';
 
 const Layout = () => {
+  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+
+  const handleResize = () => {
+    setViewportHeight(window.innerHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <S.Container>
+    <S.Container style={{ minHeight: viewportHeight }}>
       <Header />
       <img src={BannerImg} width="100%" />
       <Outlet />
