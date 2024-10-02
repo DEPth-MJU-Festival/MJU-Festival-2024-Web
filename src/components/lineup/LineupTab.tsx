013a1acd6062f09635fe0421890fc0d869530e09
rlotr02 <0314kelee@naver.com>
2024-10-03 03:36:48 +0900

    feat: 이미지 로드 수정

--- src/components/lineup/LineupTab.tsx
+++ src/components/lineup/LineupTab.tsx
@@ -1,20 +1,14 @@
 import { LineupItem } from '@constants/lineup';
 import * as S from '@styles/lineup/LineupTabStyle';
-import { forwardRef, useEffect, useState } from 'react';
+import { forwardRef, useState } from 'react';
 import ImgSwitcher from '../common/ImgSwitcher';
 import NoticeTitleField from '../common/NoticeTitleField';
 
 const LineupTab = forwardRef<HTMLDivElement>((_, ref) => {
   const [selectedId, setSelectedId] = useState(0);
+
   const isPrevDisabled = selectedId === 0;
   const isNextDisabled = selectedId === LineupItem.length - 1;
-
-  useEffect(() => {
-    LineupItem.forEach(item => {
-      const img = new Image();
-      img.src = item.img;
-    });
-  }, []);
 
   const handlePrev = () => {
     setSelectedId(prev => Math.max(prev - 1, 0));
@@ -25,7 +19,7 @@ const LineupTab = forwardRef<HTMLDivElement>((_, ref) => {
   };
 
   return (
-    <div ref={ref}>
+    <div ref={ref} style={{ minHeight: 500 }}>
       <ImgSwitcher
         img={LineupItem[selectedId].img}
         handlePrev={handlePrev}
