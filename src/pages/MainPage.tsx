fc2f16df3c8e615244af16e6d473da0c272d54ad
rlotr02 <0314kelee@naver.com>
2024-10-02 15:47:12 +0900

    feat: 이벤트 Tab UI 제작

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -8,6 +8,7 @@ import LineupTab from '@components/lineup/LineupTab';
 import BoothFoodTab from '@components/boothfood/BoothFoodTab';
 import FleaMarketTab from '@components/fleamarket/FleaMarketTab';
 import AffiliateBarTab from '@components/affiliatebar/AffiliateBarTab';
+import EventTab from '@/components/event/EventTab';
 
 const MainPage = () => {
   const [selectedBar, setSelectedBar] = useState(0);
@@ -60,6 +61,8 @@ const MainPage = () => {
         );
       case 4:
         return <FleaMarketTab ref={el => (tabRefs.current[4] = el)} />;
+      case 5:
+        return <EventTab ref={el => (tabRefs.current[5] = el)} />;
       case 6:
         return <AffiliateBarTab ref={el => (tabRefs.current[6] = el)} />;
       default:
